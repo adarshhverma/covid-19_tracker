39900401722ed2b1f1907e71d5a6b30cb9418b26
Adarsh Verma <averma6239@gmail.com>
2020-10-19 21:57:17 +0530

    Countries added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import {
   MenuItem,
@@ -7,17 +7,42 @@ import {
 } from "@material-ui/core";
 
 function App() {
+
+  const [countries, setcountries] = useState([])
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries").then((response) => response.json()).then((data) => {
+      const countries = data.map((country) => (
+        {
+          name: country.country,
+          value: country.countryInfo.iso2
+        }
+      ));
+
+      setcountries(countries);
+
+      })
+    }
+
+      getCountriesData();
+
+  }, []);
   return (
     <div className="app">
-      <h1>Covid-19 Tracker</h1>
-        <FormControl className="app__dropdown">
-          <Select>
-            <MenuItem value="worldwide">WorldWide</MenuItem>
-            <MenuItem value="worldwide">WorldWide</MenuItem>
-            <MenuItem value="worldwide">WorldWide</MenuItem>
-            <MenuItem value="worldwide">WorldWide</MenuItem>
-          </Select>
-        </FormControl>
+      <div className="app_header">
+        <h1>Covid-19 Tracker</h1>
+          <FormControl className="app__dropdown">
+            <Select>
+              {
+                countries.map(country => (
+                  <MenuItem value={country.value}>{country.name}</MenuItem>
+                ))
+              }
+
+            </Select>
+          </FormControl> 
+      </div>
     </div>
       
   );
