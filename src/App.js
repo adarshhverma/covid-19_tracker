888eecf4cda88e41ee4a569c8053497f60403979
Adarsh Verma <averma6239@gmail.com>
2020-10-20 02:55:51 +0530

    structuring done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import InfoBox from './infoBox';
+import Map from './map'
 import './App.css';
 import {
   MenuItem,
@@ -8,7 +10,8 @@ import {
 
 function App() {
 
-  const [countries, setcountries] = useState([])
+  const [countries, setcountries] = useState([]);
+  const [country, setCountry] = useState('worldwide');
 
   useEffect(() => {
     const getCountriesData = async () => {
@@ -22,26 +25,51 @@ function App() {
 
       setcountries(countries);
 
+      
+
       })
     }
 
       getCountriesData();
 
   }, []);
+
+  const onCountryChange = async (event)=> {
+    const countryCode = event.target.value;
+    console.log("Yooo",countryCode);
+    setCountry(countryCode);
+  }
+
+  
+
   return (
     <div className="app">
-      <div className="app_header">
-        <h1>Covid-19 Tracker</h1>
-          <FormControl className="app__dropdown">
-            <Select>
-              {
-                countries.map(country => (
-                  <MenuItem value={country.value}>{country.name}</MenuItem>
-                ))
-              }
+    <div className='app_left'>
+        <div className="app_header">
+          <h1>Covid-19 Tracker</h1>
+            <FormControl className="app__dropdown">
+              <Select variant='outlined' onChange={onCountryChange} value={country}>
+                <MenuItem value='worldwide'>Worldwide</MenuItem>
+                {
+                  countries.map(country => (
+                    <MenuItem value={country.value}>{country.name}</MenuItem>
+                  ))
+                }
 
-            </Select>
-          </FormControl> 
+              </Select>
+            </FormControl> 
+            
+        </div>
+        <div className="app__stats">
+          <InfoBox title='Corona Cases' total={3000}  cases={1234}/>
+          <InfoBox title='Recovered' total={2000} cases={1234}/>
+          <InfoBox title='Deaths' total={7000} cases={1234}/>
+        
+        </div>
+
+        <Map/>
+        <h2>{country}</h2>
+
       </div>
     </div>
       
